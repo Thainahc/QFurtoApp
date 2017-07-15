5062fd0b1498f6fd8ca366efd24301de7f259310
Thainá Helena <thainahc3@gmail.com>
2017-07-14 21:11:08 -0300

    First commit

---
+++ QFurtoApp/www/App/Js/common.js
@@ -0,0 +1,40 @@
+﻿function onLoad() {
+    parseToCurrency();
+    parseToDecimal();
+    parseToDecimalThree();
+    parseToPercent();
+}
+
+function parseToCurrency() {
+    $(".currency").priceFormat({
+        prefix: "R$ ",
+        centsSeparator: ",",
+        thousandsSeparator: "."
+    });
+}
+
+function parseToDecimal() {
+    $(".decimal").priceFormat({
+        prefix: "",
+        centsSeparator: ",",
+        thousandsSeparator: "."
+    });
+}
+
+function parseToDecimalThree() {
+    $(".decimalThree").priceFormat({
+        prefix: "",
+        centsSeparator: ",",
+        thousandsSeparator: ".",
+        centsLimit: 3
+    });
+}
+
+function parseToPercent() {
+    $(".percent").priceFormat({
+        prefix: "",
+        suffix: "%",
+        centsSeparator: ",",
+        thousandsSeparator: "."
+    });
+}
---
+++ QFurtoApp/www/App/Js/principal.js
@@ -0,0 +1,21 @@
+﻿function carregaPrincipal() {
+    loadPrincipal();
+}
+
+function loadPrincipal(){
+    montaViewPrincipal();
+}
+
+function montaViewPrincipal() {
+    var html = '';
+
+    html += '<nav class="navbar navbar-default">';
+
+    html += '</nav>'
+
+    html += '<div id="principal" class="container-fluid">';
+    html += '</div>';
+
+    $("#container").empty();
+    $("#container").append(html);
+}
